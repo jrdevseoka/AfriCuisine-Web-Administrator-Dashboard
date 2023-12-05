f8ccdafe4c8287e8c0ab192eb3969d9149678457
Seoka Moshele Kabelo <seokamk@gmail.com>
2023-12-06 00:33:51 +0200

    Login Component: Implement OnSubmit() Method

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/services/auth.service";
+import { AuthCommand } from "src/app/shared/commands/auth.command";
+import { AuthResponse } from "src/app/shared/res/auth.response";
 
 @Component({
   selector: 'app-login',
@@ -7,15 +11,57 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 })
 export class LoginComponent {
   loginForm: FormGroup
-  constructor(private fb: FormBuilder) {
+
+  processed: boolean;
+  reponse: AuthResponse;
+  submitting: boolean
+  constructor(
+    private fb: FormBuilder,
+    private readonly auth: AuthService,
+    private router: Router
+  ) {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
     })
 
+    this.processed = false
+    this.submitting = false
+    this.reponse = { token: '', succeeded: false }
   }
-  onSubmit()
-  {
+  onSubmit() {
+    this.submitting = true
+    this.processed = false
 
+    this.checkFormValidity(this.loginForm.invalid)
+    const command = this.mapToAuthCommand(this.loginForm)
+    this.auth.signInWithEmailAndPassword(command).subscribe((res) => {
+       this.reponse = res
+       if(res.succeeded)
+       {
+        this.submitting = false
+        this.processed = true
+        this.router.navigate(['dashboard']);
+       }
+       this.submitting = false
+       this.processed = true
+    })
+
+  }
+  private checkFormValidity(valid: boolean) {
+    if (valid) {
+      this.reponse.message = 'Invalid username or password';
+      this.reponse.succeeded = false;
+      this.submitting = false;
+      this.processed = true;
+      return;
+    }
+  }
+  private mapToAuthCommand(form: FormGroup) {
+    const command: AuthCommand = {
+      password: form.get('password')?.value,
+      username: form.get('username')?.value,
+    }
+    return command
   }
 }
