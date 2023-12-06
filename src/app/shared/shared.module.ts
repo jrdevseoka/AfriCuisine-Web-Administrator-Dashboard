d26564328e60e080ea153406bf6a7fdcae22b659
Seoka Moshele Kabelo <seokamk@gmail.com>
2023-12-06 17:15:33 +0200

    Dashboard: Added A Preloader

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { ErrorAlertComponent } from "./alerts/error/error-alert.component";
+import { PreloaderComponent } from "./components/preloader/preloader.component";
 
 @NgModule({
-  declarations: [ErrorAlertComponent],
+  declarations: [ErrorAlertComponent, PreloaderComponent],
   imports: [CommonModule, ReactiveFormsModule],
-  exports: [ErrorAlertComponent]
+  exports: [ErrorAlertComponent, PreloaderComponent]
 })
-export class SharedComponentModule{}
+export class SharedComponentModule { }
 
