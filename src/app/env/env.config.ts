9f499153781d1c54e910e8a610893b2bb8a1e3eb
Seoka Moshele Kabelo <seokamk@gmail.com>
2023-12-23 05:10:50 +0200

    Profile-Upload Working

--- src/app/env/env.config.ts
+++ src/app/env/env.config.ts
@@ -1,5 +1,5 @@
 export const enviroment = {
-  server: 'http://localhost:5214/api/1.0',
-   host: 'https://localhost:4200/',
-   message:  'Please try again, if the issue persist contact our administrator on support@africuisine.app'
+  server: 'https://localhost:7114/api/1.0',
+  host: 'https://localhost:4200/',
+  message: 'Please try again, if the issue persist contact our administrator on support@africuisine.app'
 }
